4067b1ba6cdc80eec330db79363bd026b7774c69
Lachlan Foy <lachie.foy7@gmail.com>
2022-06-21 21:59:16 +1000

    sdl boilerplate

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,73 @@
-#include <iostream>
+#include <stdio.h>
+#include <SDL2/SDL.h>
 
-int main()
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
+
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+
+bool init()
 {
-    std::cout << "main!\n";
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        printf("Unable to initialize SDL: %s\n", SDL_GetError());
+        return false;
+    }
+
+    window = SDL_CreateWindow("", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+    if(window == NULL)
+    {
+        printf("Window could not be created: %s\n", SDL_GetError());
+        return false;
+    }
+
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if(window == NULL)
+    {
+        printf("Renderer could not be created: %s\n", SDL_GetError());
+        return false;
+    }
+
+    return true;
+}
+
+void cleanUp()
+{
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
+}
+
+int main(int argc, char* argv[])
+{
+    if(init())
+    {
+        SDL_Rect rect = { 100, 100, 50, 50 };
+        
+        SDL_Event e;
+        bool quit = false;
+        while(!quit)
+        {
+            while(SDL_PollEvent(&e) != 0)
+            {
+                if(e.type == SDL_QUIT)
+                {
+                    quit = true;
+                }
+            }
+
+            SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
+            SDL_RenderClear(renderer);
+            SDL_SetRenderDrawColor(renderer, 0x00, 0x11, 0xaa, 0xff);
+            SDL_RenderFillRect(renderer, &rect);
+
+            SDL_RenderPresent(renderer);
+        }
+    }
+
+    cleanUp();
+
     return 0;
 }
