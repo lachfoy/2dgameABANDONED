2510786093dae9a7ae317b0f135200ef18042943
Lachlan Foy <lachie.foy7@gmail.com>
2022-07-09 22:57:15 +1000

    not not sure how to handle damage without overcomplicating

--- include/DynamicHealthbar.h
+++ include/DynamicHealthbar.h
@@ -23,8 +23,8 @@ void DynamicHealthbar::updatePosition()
     int newX = (int)damageable->posX;
     int newY = (int)damageable->posY - damageable->getHeight() - height - 2; // newY - objH - healthbarH - some padding value
 
-    this->x = newX - (int)(length / 2);
-    this->y = newY - (int)(height / 2);
+    this->x = newX - (int)(length / 2.0f);
+    this->y = newY - (int)(height / 2.0f);
 
     bg_rect.x = this->x;
     bg_rect.y = this->y;
