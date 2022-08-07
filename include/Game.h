dc2d4b0a9db23737be6881d88bafd172f41ef451
lachfoy <lachie.foy7@gmail.com>
2022-08-07 12:08:22 +1000

    organisation

--- include/Game.h
+++ include/Game.h
@@ -1,26 +1,36 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include <stdio.h>
 #include <vector>
 
 #include <SDL2/SDL.h>
 
-#include "Engine.h"
-
-#define DEBUG_DRAW 0
-
+class BitmapFont;
+class InputManager;
+class ResourceManager;
 class Player;
 class ProjectileManager;
 class EnemyManager;
 class UiManager;
 class ParticleManager;
 
-class Game : public Engine
+class Game
 {
 public:
     Game();
 
+    bool init(int w, int h); // build the window and renderer
+    void run(); // start game loop
+
 private:
+    int windowWidth;
+    int windowHeight;
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    BitmapFont* bitmapFont;
+    InputManager* inputManager;
+    ResourceManager* resourceManager;
     Player* player;
     ProjectileManager* projectileManager;
     EnemyManager* enemyManager;
@@ -28,9 +38,10 @@ private:
     ParticleManager* particleManager;
 
     void onCreate();
-    void onCleanup();
+    void onDestroy(); // deallocate window and renderer
     void onUpdate(float dt);
     void onRender();
+
 };
 
 #endif
